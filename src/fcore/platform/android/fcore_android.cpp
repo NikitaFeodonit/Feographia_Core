a952c3c6f687d5f64ef7f53a1258e031f75fd998
NikitaFeodonit <nikitafeodonit@yandex.com>
2015-06-12 22:42:48 +0200

    Redirect the "stdout" to android log

--- src/fcore/platform/android/fcore_android.cpp
+++ src/fcore/platform/android/fcore_android.cpp
@@ -21,6 +21,8 @@
 
 #include <jni.h>
 #include <zmq.hpp>
+#include <streambuf>
+#include <iostream>
 
 #include <android/log.h>
 #define LOG(fmt, ...) __android_log_print(ANDROID_LOG_DEBUG, "fcore", fmt, ##__VA_ARGS__)
@@ -28,13 +30,48 @@
 #include "fcore/fcore.hpp"
 
 
+// http://stackoverflow.com/a/8870278
+// redirect the "stdout" to android log
+class androidbuf : public std::streambuf {
+public:
+    enum { bufsize = 128 }; // ... or some other suitable buffer size
+    androidbuf() { this->setp(buffer, buffer + bufsize - 1); }
+
+private:
+    int overflow(int c)
+    {
+        if (c == traits_type::eof()) {
+            *this->pptr() = traits_type::to_char_type(c);
+            this->sbumpc();
+        }
+        return this->sync()? traits_type::eof(): traits_type::not_eof(c);
+    }
+
+    int sync()
+    {
+        int rc = 0;
+        if (this->pbase() != this->pptr()) {
+            char writebuf[bufsize+1];
+            memcpy(writebuf, this->pbase(), this->pptr() - this->pbase());
+            writebuf[this->pptr() - this->pbase()] = '\0';
+
+            rc = __android_log_write(ANDROID_LOG_INFO, "std", writebuf) > 0;
+            this->setp(buffer, buffer + bufsize - 1);
+        }
+        return rc;
+    }
+
+    char buffer[bufsize];
+};
+
+
 extern "C"
 jlong Java_ru_feographia_FCore_fcoreRunMainThread(JNIEnv* env, jobject thiz)
 {
+    std::cout.rdbuf(new androidbuf);
+
     LOG("main thread: %s", "starting");
-
     jlong zmqContextPointer = (jlong) fcoreRunMainThread();
-
     LOG("main thread: %s", "started");
 
     return zmqContextPointer;
